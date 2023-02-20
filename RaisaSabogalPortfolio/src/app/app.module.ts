f82fe2a61f97080149e3c7fc76d792e9e0ce209b
RaisaSabogal <111298930+RaisaSabogal@users.noreply.github.com>
2023-02-19 22:56:50 -0300

    .

--- RaisaSabogalPortfolio/src/app/app.module.ts
+++ RaisaSabogalPortfolio/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { APComponent } from './components/ap/ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    APComponent
+    APComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
