c73132444638c1cac5841e2e297c795a245cd69c
RaisaSabogal <111298930+RaisaSabogal@users.noreply.github.com>
2023-02-19 21:22:48 -0300

    .

--- RaisaSabogalPortfolio/src/app/app.module.ts
+++ RaisaSabogalPortfolio/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { APComponent } from './components/ap/ap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    APComponent
   ],
   imports: [
     BrowserModule,
---
+++ RaisaSabogalPortfolio/src/app/components/ap/ap.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ap',
+  templateUrl: './ap.component.html',
+  styleUrls: ['./ap.component.css']
+})
+export class APComponent {
+
+}
